ef2588b04d60d17ebb4aa02a9de33a48064f8019
Giseli Azevedo <vazevedo73@gmail.com>
2025-06-19 18:54:22 -0300

    .

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function LandingPage() {
   return (
     <div className="bg-[#dddddd] text-[#74726e] font-[Arial,sans-serif]">
-      <header className="bg-[#ffffff] relative w-full h-[500px] text-center text-white justify-center flex flex-col">
+      <header className="bg-[#ffffff] relative w-full h-[500px] text-center text-white justify-center flex flex-col content-between">
         <img src="https://i.ibb.co/Kc8bgqkC/png-4.png" alt="" className="object-cover w-full h-full" />
         {/* <h1 className="text-4xl md:text-5xl font-bold mb-4" style={{ fontFamily: 'Felix Titling, serif' }}>
           Evite prejuízos com infiltrações
