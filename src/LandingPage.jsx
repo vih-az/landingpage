8a8afb6a3cc0c19869af928ba551c9a7afbe7e9f
Giseli Azevedo <vazevedo73@gmail.com>
2025-06-19 17:43:14 -0300

    .[

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function LandingPage() {
   return (
     <div className="bg-[#dddddd] text-[#74726e] font-[Arial,sans-serif]">
-      <header className="bg-[url(https://i.ibb.co/v404zfC8/Captura2-de-tela-2025-06-18-214637.jpg)] bg-size-[96] bg-no-repeat text-center text-white h-96">
+      <header className="bg-[url(https://i.ibb.co/v404zfC8/Captura2-de-tela-2025-06-18-214637.jpg)] bg-cover bg-no-repeat text-center text-white h-96">
         {/* <img src="https://i.ibb.co/C5dpqxMx/Captura-de-tela3-2025-06-18-214637.jpg" alt="" className="w-full h-90%" /> */}
         {/* <h1 className="text-4xl md:text-5xl font-bold mb-4" style={{ fontFamily: 'Felix Titling, serif' }}>
           Evite prejuízos com infiltrações
