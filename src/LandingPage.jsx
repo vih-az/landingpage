5fb9f326a20b5f6728cb6f861f0ce02e466d9c73
Giseli Azevedo <vazevedo73@gmail.com>
2025-06-19 23:41:57 -0300

    .

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -6,7 +6,7 @@ export default function LandingPage() {
   return (
     <div className="bg-[#dddddd] text-[#74726e] font-[Arial,sans-serif]">
       <header className="bg-[#ffffff] relative w-full h-[500px] text-center text-white justify-center flex flex-col content-between">
-        <img src="https://i.ibb.co/0VvJ6Qjv/png-png.png" alt="" className="object-cover w-full h-full" />
+        <img src="https://i.ibb.co/6R3w7Kxj/png-png-1.png" alt="" className="object-cover w-full h-full" />
         {/* <h1 className="text-4xl md:text-5xl font-bold mb-4" style={{ fontFamily: 'Felix Titling, serif' }}>
           Evite prejuízos com infiltrações
         </h1>
