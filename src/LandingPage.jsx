6804abb2b06bbe039e5bb8ac59a24fb6d576f2e9
Giseli Azevedo <vazevedo73@gmail.com>
2025-06-24 16:34:57 -0300

    .

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -27,7 +27,7 @@ export default function LandingPage() {
         </div>
       </header>
 
-      <section className="py-12 px-6 max-w-4xl mx-auto">
+      <section className="py-12 px-6 max-w-4xl mx-auto flex">
         <div>
             <h2 className="text-2xl font-semibold mb-4" style={{ fontFamily: 'Felix Titling, serif' }}>Por que</h2>
             <h2 className="text-2xl font-semibold mb-4" style={{ fontFamily: 'Felix Titling, serif' }}>Impermeabilização</h2>
