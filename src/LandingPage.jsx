a42aef849fbbcee0fd6ffea7bc7cf2d4fe0352fb
Giseli Azevedo <vazevedo73@gmail.com>
2025-07-04 13:03:48 -0300

    .

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -94,7 +94,7 @@ export default function LandingPage() {
           <div>
             <h2 className="text-2xl font-semibold mb-4 text-[#0d68c9]" style={{ fontFamily: 'Felix Titling, serif' }}>Fundadora</h2>
             <h2>Engª Esp. Giseli Azevedo</h2>
-            <h2>Especialista em patologias da construção, com sólida atuação em consultorias, desenvolvimento de soluções técnicas e participação ativa em normas da ABNT e do IBI.</h2>
+            <h2 className="w-52">Especialista em patologias da construção, com sólida atuação em consultorias, desenvolvimento de soluções técnicas e participação ativa em normas da ABNT e do IBI.</h2>
           </div>
           <div>
             <ul className="gap-2 flex flex-col">
