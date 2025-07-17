1a5c4e4a53d45cdd7773260232d5dc7372d6aadd
Giseli Azevedo <vazevedo73@gmail.com>
2025-07-17 11:16:02 -0300

    .

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -142,7 +142,7 @@ export default function LandingPage() {
 
       <section className="py-12 px-6 mx-auto bg-[#0a3062] text-white w-ful flex flex-col justify-center items-center">
         <div>
-          <div>
+          <div className="flex">
             <h2>O que oferecemos</h2>
             <h2>Oferecemos um processo consultivo completo em impermeabilização:</h2>
           </div>
