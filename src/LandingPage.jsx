c3e3c0a250ec6716c426a180e83526393207d8cc
Giseli Azevedo <vazevedo73@gmail.com>
2025-07-21 17:15:00 -0300

    .

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -233,7 +233,7 @@ export default function LandingPage() {
           </div>
         </div>
       </section>
-      <section className="py-12 px-6 mx-auto bg-[#ffffff] w-full flex flex-col justify-center items-center">
+      <section className="py-12 px-6 mx-auto bg-[#0a3062] w-full flex flex-col justify-center items-center">
        <div className="w-[80%]">
           <div>
             <h2 className="text-2xl font-semibold mb-4 text-[#0d68c9] justify-self-start" style={{ fontFamily: 'Felix Titling, serif' }}>Cases e</h2>
