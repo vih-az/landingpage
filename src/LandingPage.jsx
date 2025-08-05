2a92583cb60e99edc08df26b23502489a1946f74
Giseli Azevedo <vazevedo73@gmail.com>
2025-08-05 12:54:48 -0300

    .

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -24,7 +24,7 @@ export default function LandingPage() {
       </header>
 <section className=" mx-auto bg-[#0a3062] w-full h-[34rem] flex flex-col justify-center items-center bg-[url(https://i.postimg.cc/jjsVDgxs/Design-sem-nome-4-1.png)] text-white bg-no-repeat bg-center bg-cover">
        <div className=" flex flex-col gap-2 w-full h-full z-10 bg-linear-[90deg,#0A3062_0%,#FFFFFF_100%]">
-        <div className="w-[50%] h-full flex flex-col justify-center items-center">
+        <div className="w-full h-[50%] flex flex-col justify-center items-center md:w-[50%] md:h-full">
           <h1 className="text-3xl">A impermeabilização é investimento, não despesa</h1>
           <h1>Giseli Azevedo</h1>
         </div>
