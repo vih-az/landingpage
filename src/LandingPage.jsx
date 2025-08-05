10f1645116dfbded6ab119b2750f3506e2f2d296
Giseli Azevedo <vazevedo73@gmail.com>
2025-08-05 16:18:24 -0300

    .

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -250,10 +250,12 @@ export default function LandingPage() {
         </div>
       </section>
       <section className="py-12 px-6 mx-auto bg-[#0a3062] w-full flex flex-col justify-center items-center bg-[#0a3062] text-white">
+        <div>
+          <h2 className="text-2xl font-semibold mb-4 text-[#0d68c9] justify-self-start" style={{ fontFamily: 'Felix Titling, serif' }}>Cases e</h2>
+          <h2 className="text-2xl font-semibold mb-4 text-[#0d68c9] justify-self-start" style={{ fontFamily: 'Felix Titling, serif' }}>Resultados</h2>
+        </div>
        <div className=" flex flex-col gap-2">
           <div>
-            <h2 className="text-2xl font-semibold mb-4 text-[#0d68c9] justify-self-start" style={{ fontFamily: 'Felix Titling, serif' }}>Cases e</h2>
-            <h2 className="text-2xl font-semibold mb-4 text-[#0d68c9] justify-self-start" style={{ fontFamily: 'Felix Titling, serif' }}>Resultados</h2>
             <h2>Mais de 100 projetos realizados com excelência em SP, RJ e MG.</h2>
           </div>
           <div className="flex gap-2">
