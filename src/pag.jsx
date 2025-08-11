d10747cd0a4a038255bea69082f832698e7672a4
Giseli Azevedo <vazevedo73@gmail.com>
2025-08-11 18:28:43 -0300

    .

---
+++ src/pag.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Pagina from './src/Pagina';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('pagina')).render(
+  <React.StrictMode>
+    <Pagina />
+  </React.StrictMode>
+);
